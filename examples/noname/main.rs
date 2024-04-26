7501c5f8d627e2df6bf7bb0ae9eff3210c2b67a3
Jibao Mansaray <jibaomansaray@gmail.com>
2024-04-26 05:36:37 +0000

    example: add examples for `merge` and `noname` routes

---
+++ examples/noname/main.rs
@@ -0,0 +1,24 @@
+use axum::{response::IntoResponse, routing::get};
+use named_routes_axum::RouterWrapper;
+
+#[tokio::main]
+async fn main() {
+    let app = RouterWrapper::new()
+        .get("/", home_handler, "home") // 1. The home route has a name
+        .route("/noname", get(noname_route)); // 2. This route does not have a name
+
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
+        .await
+        .unwrap();
+
+    println!("listening on {}", listener.local_addr().unwrap());
+    axum::serve(listener, app.into_router()).await.unwrap();
+}
+
+async fn home_handler() -> impl IntoResponse {
+    "Welcome"
+}
+
+async fn noname_route() -> impl IntoResponse {
+    "This route does not have a name"
+}
